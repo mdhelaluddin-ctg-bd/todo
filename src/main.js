de46e5ccbec951336801a7a81eb1fda376e7c6ca
Md Helal Uddin <mdhelaluddin.ctg.bd@gmail.com>
2022-01-23 23:16:26 +0600

    configured graphQL and added userQuery plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, /* createHttpLink, */ InMemoryCache } from '@apollo/client/core'
 
-createApp(App).mount('#app')
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    uri: 'https://api.graphqlplaceholder.com/',
+    cache: new InMemoryCache()
+})
+
+createApp({
+    setup () {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    
+    render: () => h(App)
+    
+}).mount('#app')
